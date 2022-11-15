9b59907c3e2d98ade8e4a65bfbb645934abd24f5
wanuch <wanuch.int@gmail.com>
2022-11-15 21:49:03 +0700

    data error handling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,24 @@
 import path from "path";
 
-export async function getStaticProps() {
+export async function getStaticProps(context: any) {
   console.log("(Re-)Generating...");
   const fs = require('fs').promises;
 
   const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: "/no-data"
+      }
+    }
+  }
+
+  if (data.products.length === 0) {
+    return { notFound: true };
+  }
 
   return {
     props: {
