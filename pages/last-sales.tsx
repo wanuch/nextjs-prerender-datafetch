914b0a09c3c29b7cbd6b9857b7380d58e5b6336c
wanuch <wanuch.int@gmail.com>
2022-11-16 19:01:15 +0700

    useSWR

--- pages/last-sales.tsx
+++ pages/last-sales.tsx
@@ -1,36 +1,62 @@
 import { useEffect, useState } from "react";
+import useSWR from 'swr';
 
 export default function LastSales(props: any) {
     const [sales, setSales] = useState<any>();
-    const [isLoading, setIsLoading] = useState<boolean>(false);
+
+    // ========================= With useSWR ============================ 
+    const fetcher = (url: any) => fetch(url).then((r) => r.json());
+    const { data, error } = useSWR("https://react-getting-start-62baa-default-rtdb.asia-southeast1.firebasedatabase.app/sales.json", fetcher);
 
     useEffect(() => {
-        setIsLoading(true);
-        fetch("https://react-getting-start-62baa-default-rtdb.asia-southeast1.firebasedatabase.app/sales.json")
-            .then((response: any) => response.json())
-            .then(data => {
-                const transformedSales = [];
+        if (data) {
+            const transformedSales = [];
 
-                for (const key in data) {
-                    transformedSales.push({
-                        id: key,
-                        username: data[key].username,
-                        volume: data[key].volume
-                    });
-                }
+            for (const key in data) {
+                transformedSales.push({
+                    id: key,
+                    username: data[key].username,
+                    volume: data[key].volume
+                });
+            }
+            setSales(transformedSales);
+        }
+    }, [data]);
 
-                setSales(transformedSales);
-                setIsLoading(false);
-            });
-    }, []);
+    if (error) return <div>failed to load</div>
+    if (!data || !sales) return <div>Loading...</div>
+    // ========================= With useSWR ============================ 
 
-    if (isLoading) {
-        return <p>Loading...</p>
-    }
+    // ========================= Without useSWR =========================
+    // const [isLoading, setIsLoading] = useState<boolean>(false);
+    // useEffect(() => {
+    //     setIsLoading(true);
+    //     fetch("https://react-getting-start-62baa-default-rtdb.asia-southeast1.firebasedatabase.app/sales.json")
+    //         .then((response: any) => response.json())
+    //         .then(data => {
+    //             const transformedSales = [];
 
-    if (!sales) {
-        return <p>No data yet</p>
-    }
+    //             for (const key in data) {
+    //                 transformedSales.push({
+    //                     id: key,
+    //                     username: data[key].username,
+    //                     volume: data[key].volume
+    //                 });
+    //             }
+
+    //             setSales(transformedSales);
+    //             setIsLoading(false);
+    //         });
+    // }, []);
+
+    // if (isLoading) {
+    //     return <p>Loading...</p>
+    // }
+
+    // if (!sales) {
+    //     return <p>No data yet</p>
+    // }
+    // ========================= Without useSWR ========================= 
 
     return (
         <ul>
